2654609adeb87178648ec57b0e26b88c1ed3223c
andikaarif <andikapradanaarif@gmail.com>
2022-03-09 17:41:36 +0700

    test commit linter

--- src/playground/pandadoc/main.js
+++ src/playground/pandadoc/main.js
@@ -5,16 +5,22 @@ const documentCreateRequest = require('./sample2');
 const { SYSTEM_ENV } = require('../../config/env');
 
 async function main() {
-  const configuration = pandadoc
-    .createConfiguration({ authMethods: { apiKey: SYSTEM_ENV.PANDADOC_API_KEY } });
+  const configuration = pandadoc.createConfiguration({
+    authMethods: { apiKey: SYSTEM_ENV.PANDADOC_API_KEY },
+  });
   const apiDocument = new pandadoc.DocumentsApi(configuration);
   const document = await apiDocument.createDocument({ documentCreateRequest });
   console.log(document.infoMessage);
+
   for (let i = 0; i < 4; i++) {
     await new Promise((rs) => setTimeout(rs, 1000));
-    const documentStatus = await apiDocument.detailsDocument({ id: document.id });
+    const documentStatus = await apiDocument.detailsDocument({
+      id: document.id,
+    });
     console.log(documentStatus);
-    if (documentStatus.status === 'draft') { break; }
+    if (documentStatus.status === 'draft') {
+      break;
+    }
   }
   const link = await apiDocument.sendDocument({
     id: document.id,
